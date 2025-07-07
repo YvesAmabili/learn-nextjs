7488d0bf734a0cf03860bb4d508e056fba3c2e6a
yves.amabili <yves.amabili@totalenergies.be>
2025-07-07 17:02:16 +0200

    Setup assets & metadata

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,10 @@ const Home = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello World</h1>
+
+      <span className="text-4xl font-space-grotesk font-bold mt-2 inline-block">
+        DevOverflow
+      </span>
     </div>
   );
 };
