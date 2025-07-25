98584965a4d050baa781430dea93addc19ffcd15
yves.amabili <yves.amabili@totalenergies.be>
2025-07-25 11:11:05 +0200

    Implement get user by email

---
+++ app/api/users/email/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import User from "@/database/user.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import { UserSchema } from "@/lib/validations";
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const { email } = body;
+
+    const validatedFields = UserSchema.partial().safeParse(body);
+    if (!validatedFields.success)
+      throw new ValidationError(validatedFields.error.flatten().fieldErrors);
+
+    const user = await User.findOne({ email });
+    if (!user) throw new NotFoundError("User");
+
+    return NextResponse.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
